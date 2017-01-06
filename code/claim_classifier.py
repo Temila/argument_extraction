f6a7c2b66913bc09af42d2c6eab3d773c5a03443
Temila <temila.ms06f@gmail.com>
2017-01-06 19:24:39 +0100

    add sequence component

--- code/claim_classifier.py
+++ code/claim_classifier.py
@@ -1,28 +1,31 @@
 import xgboost as xgb
 import pandas as pd
 import numpy as np
+from util import generate_train_test
 
-data_df = pd.read_csv('data.csv',header = 0)
+# data_df = pd.read_csv('data.csv',header = 0)
 # true_label = data_df.loc[data_df['Label'] == 1]
 # for i in range(35):
 #     data_df = data_df.append(true_label)
-test_df = pd.read_csv('test.csv',header = 0)
+# test_df = pd.read_csv('test.csv',header = 0)
 # true_label = test_df.loc[test_df['Label'] == 1]
 # for i in range(35):
 #     test_df = test_df.append(true_label)
-features = ['Mas','Ecs_1','Ecs_2','Ecs_3','Cnf_1','Cnf_2','Cnf_3','Cnf_4','Sub','Sen']
+data_df = pd.read_csv('data/sentence_component.csv',header = 0)
+train_df, test_df = generate_train_test(data_df)
+features = ['Mas','Ecs_1','Ecs_2','Ecs_3','Cnf_1','Cnf_2','Cnf_3','Cnf_4','Sub','Sen','sq1','sq2','sq3','sq4','sq5','sq6','sq7']
 label = ['Label']
-dtrain = xgb.DMatrix(data_df[features],data_df[label])
-dtest = xgb.DMatrix(test_df[features],data_df[label])
+dtrain = xgb.DMatrix(train_df[features],train_df[label])
+dtest = xgb.DMatrix(test_df[features],test_df[label])
 param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'binary:logistic'}
 label = dtrain.get_label()
 ratio = float(np.sum(label == 0)) / np.sum(label==1)
 param['scale_pos_weight'] = ratio
 num_round = 100
-# res = xgb.cv(param, dtrain, num_round, nfold=10,
-#        metrics={'error'}, seed = 0,
-#        callbacks=[xgb.callback.print_evaluation(show_stdv=True)])
-# print res
+res = xgb.cv(param, dtrain, num_round, nfold=10,
+       metrics={'error'}, seed = 0,
+       callbacks=[xgb.callback.print_evaluation(show_stdv=True)])
+print res
 bst = xgb.train(param, dtrain, num_round)
 ptrain = bst.predict(dtrain, output_margin=True)
 ptest  = bst.predict(dtest, output_margin=True)
--- code/util.py
+++ code/util.py
@@ -1,4 +1,6 @@
 from prefixspan import prefixspan
+import pandas as pd
+import numpy as np
 
 def match_sequences(encoded_sentence, sequences):
     pfs = prefixspan([encoded_sentence])
@@ -13,3 +15,20 @@ def match_sequences(encoded_sentence, sequences):
         else:
             features.append(0)
     return features
+
+
+def generate_train_test(data_df):
+    # data_df = pd.read_csv('data/sentence_component.csv',header = 0)
+    true = data_df.loc[data_df['Label'] == 1]
+    false = data_df.loc[data_df['Label'] == 0]
+    train_true,test_true = _split_data(true)
+    train_false,test_false = _split_data(false)
+    train = train_false.append(train_true)
+    test = test_false.append(test_true)
+    return train, test
+
+def _split_data(data_df):
+    msk = np.random.rand(len(data_df)) < 0.8
+    train_data = data_df[msk]
+    test_data = data_df[~msk]
+    return train_data, test_data
